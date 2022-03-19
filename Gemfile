310bbe22e5539bab97e8ce1019781f9126eb065d
箭野弘輔 <yayakousuke@Kyanobook-Air.local>
2022-03-19 16:39:08 +0900

    メモアプリjsonファイル版作成

--- Gemfile
+++ Gemfile
@@ -5,7 +5,5 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem 'activerecord'
 gem 'sinatra'
-gem 'sqlite3'
 gem 'webrick'
--- memo.rb
+++ memo.rb
@@ -2,164 +2,91 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'active_record'
-require 'digest/sha2'
 require 'cgi'
+require 'json'
 
 set :environment, :production
 
-set :sessions,
-    expire_after: 7200,
-    secret: 'abcdefghij0123456789'
-
-ActiveRecord::Base.configurations = YAML.load_file('database.yml')
-ActiveRecord::Base.establish_connection :development
-
-# ユーザーテーブル
-class Users < ActiveRecord::Base
-  self.table_name = 'users'
-end
-
-# メモテーブル
-class Memos < ActiveRecord::Base
-  self.table_name = 'memos'
-end
-
-# ID管理用テーブル
-class Counts < ActiveRecord::Base
-  self.table_name = 'counts'
-end
-
 get '/' do
-  redirect '/login'
+  redirect '/memos'
 end
 
-get '/login' do
-  session[:message]
-  erb :login
-end
-
-get '/register' do
-  erb :register
-end
-
-post '/register' do
-  # サニタイズ処理
-  username = CGI.escapeHTML(params[:username])
-  pass = CGI.escapeHTML(params[:pass])
-  salt = [rand(64), rand(64)].pack('C*').tr("\x00-\x3f", 'A-Za-z0-9./')
-
-  new_user = Users.new
-  new_user.id = username
-  new_user.hashed = Digest::SHA256.hexdigest(pass + salt)
-  new_user.salt = salt
-  new_user.save
-
-  session[:message] = 'ユーザーが作成されました。'
-  redirect '/login'
-end
-
-post '/auth' do
-  username = CGI.escapeHTML(params[:username])
-  pass = CGI.escapeHTML(params[:pass])
-
-  if check_login(username, pass)
-    session[:username] = username
-    redirect '/memos/1'
-  end
-
-  session.clear
-  session[:message] = 'IDまたはパスワードが違います。'
-  redirect '/login'
-end
-
-get '/memos/:page' do
-  @user = session[:username]
-  if @user.nil?
-    session[:message] = 'ログインしてください。'
-    redirect '/login'
-  end
-
-  transition_page = params[:page].to_i
-  session[:page] = transition_page
-  @display_number = 5
-  # DBのデータを取得する位置のインデックス
-  @start_point = (transition_page - 1) * @display_number
-  # 全項目の最後の位置（Nextのページング機能をOFFにするための仕掛け）
-  @end_point = Memos.where(user_id: @user).count
-
-  @memos = Memos.where(user_id: @user).order(id: :desc).limit(5).offset(@start_point)
-
+get '/memos' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  @memos = file_context['memos']
   erb :memos
 end
 
-get '/users/:user_id/memos/new' do
-  redirect '/badrequest' if params[:user_id] != session[:username]
+get '/memos/new' do
   erb :new
 end
 
-post '/users/:user_id/memos/new' do
-  redirect '/badrequest' if params[:user_id] != session[:username]
-  # メモ全体のIDの中で一番大きいものに1加えたものをIDとする
-  max_id = Counts.find_by_id(99)
-  new_max_id = max_id.counts + 1
-  max_id.counts = new_max_id
-  max_id.save
-
-  new_memo = Memos.new
-  new_memo.id = new_max_id
-  new_memo.user_id = session[:username]
-  new_memo.title = CGI.escapeHTML(params[:title])
-  new_memo.content = CGI.escapeHTML(params[:content])
-  new_memo.save
-
-  redirect '/memos/1'
+post '/memos/new' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  new_memo =
+    if file_context['memos'].empty?
+      {
+        'id' => 1,
+        'title' => CGI.escapeHTML(params[:title]),
+        'content' => CGI.escapeHTML(params[:content])
+      }
+    else
+      {
+        'id' => file_context['memos'].last['id'] + 1,
+        'title' => CGI.escapeHTML(params[:title]),
+        'content' => CGI.escapeHTML(params[:content])
+      }
+    end
+  file_context['memos'].push(new_memo)
+  File.open('public/memos.json', 'w') { |f| JSON.dump(file_context, f) }
+  redirect '/memos'
 end
 
-get '/users/:user_id/memos/:memo_id' do
-  redirect '/badrequest' if params[:user_id] != session[:username]
-  @memo = Memos.find_by_id(CGI.escapeHTML(params[:memo_id]))
+get '/memos/:memo_id' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  memos = file_context['memos']
+  selected_memo =
+    memos.select do |memo|
+      memo['id'] == params[:memo_id].to_i
+    end
+  @target_memo = selected_memo[0]
+  erb :show
+end
+
+get '/memos/:memo_id/edit' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  memos = file_context['memos']
+  selected_memo =
+    memos.select do |memo|
+      memo['id'] == params[:memo_id].to_i
+    end
+  @target_memo = selected_memo[0]
   erb :edit
 end
 
-patch '/users/:user_id/memos/:memo_id' do
-  redirect '/badrequest' if params[:user_id] != session[:username]
-  @memo = Memos.find_by_id(params[:memo_id])
-  @memo.title = CGI.escapeHTML(params[:title])
-  @memo.content = CGI.escapeHTML(params[:content])
-  @memo.save
-  redirect '/memos/1'
+patch '/memos/:memo_id/edit' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  memos = file_context['memos']
+  target_memo_index =
+    memos.each_index.select do |index|
+      memos[index]['id'] == params[:memo_id].to_i
+    end
+  file_context['memos'][*target_memo_index]['title'] = CGI.escapeHTML(params[:title])
+  file_context['memos'][*target_memo_index]['content'] = CGI.escapeHTML(params[:content])
+  File.open('public/memos.json', 'w') { |f| JSON.dump(file_context, f) }
+  redirect '/memos'
 end
 
-delete '/users/:user_id/memos/delete' do
-  redirect '/badrequest' if params[:user_id] != session[:username]
-  delete_memo = Memos.find(params[:id])
-  delete_memo.destroy
-  redirect '/memos/1'
-# 既に消えているメモに再度デリート処理がされた時のため
-rescue ActiveRecord::RecordNotFound => e
-  p e
-  redirect '/memos/1'
-end
-
-get '/logout' do
-  session.clear
-  erb :logout
-end
-
-get '/badrequest' do
-  erb :badrequest
-end
-
-def check_login(username, pass)
-  searched_user = Users.find(username)
-  user_salt = searched_user.salt
-  user_hashed = searched_user.hashed
-  matched_hash = Digest::SHA256.hexdigest(pass + user_salt)
-  true if matched_hash == user_hashed
-rescue ActiveRecord::RecordNotFound => e
-  p e
-  false
+delete '/memos/delete' do
+  file_context = File.open('public/memos.json') { |f| JSON.parse(f) }
+  memos = file_context['memos']
+  target_memo_index =
+    memos.each_index.select do |index|
+      memos[index]['id'] == params[:memo_id].to_i
+    end
+  file_context['memos'].delete_at(*target_memo_index)
+  File.open('public/memos.json', 'w') { |f| JSON.dump(file_context, f) }
+  redirect '/memos'
 end
 
 not_found do
